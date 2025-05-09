eff858d1b140531699bec644493577a721e048f5
David <david.k.kopp.civ@mail.mil>
2025-05-08 19:09:38 -0500

    Updated installer

--- installer/macos/build-installer.sh
+++ installer/macos/build-installer.sh
@@ -2,8 +2,12 @@
 
 BASE_DIR=$(cd `dirname $0` && pwd)
 
-rm -rf ../../target/dmg
+rm -rf ${BASE_DIR}/../../target
+mkdir -p ${BASE_DIR}/../../target
 
 echo Running jpackage
-jpackage @../jpackage-common-options.txt --icon icon.icns --dest ../../target/dmg \
-         --app-version 1.0.0 --mac-package-identifier RotP --mac-app-category Games
+jpackage @${BASE_DIR}/../jpackage-common-options.txt --icon ${BASE_DIR}/icon.icns \
+          --license-file ${BASE_DIR}/../license.txt \
+          --module-path ${BASE_DIR}/../../client/target/modules \
+          --dest ${BASE_DIR}/../../target/dmg \
+          --app-version 1.0.0 --mac-package-identifier RotP --mac-app-category Games
